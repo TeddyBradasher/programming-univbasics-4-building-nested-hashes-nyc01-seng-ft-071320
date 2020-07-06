f995fecc0befb1700b68b74b7eebecb1e8cc177a
Teddy Bradsher <teddybradsher@gmail.com>
2020-07-06 02:28:11 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,14 +22,11 @@ def monopoly_with_third_tier
 }, :names => {
   :reading_railroad => {},
   :pennsylvania_railroad => {},
-  :b_and_o_railroad
+  :b_and_o_railroad => {},
+  :shortline_railroad => {}
+  } 
   
-  
-  
-  
-  
-  
-} }
+}
   }
 end
 
